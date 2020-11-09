67a839e6f199aebb8626a24dc437e6bfb33aa260
underdeveloper <der.sulthana@gmail.com>
2020-11-09 14:03:53 +0700

    Started work on Queue ADT implementation. God help my soul.

---
+++ queue.h
@@ -0,0 +1,28 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+typedef struct q_item {
+    char data;
+    struct q_item next;
+} q_item;
+
+typedef struct Queue {
+    q_item *front;
+    q_item *rear;
+} Queue;
+
+/* Initializes an empty queue */
+Queue queue();
+
+/* Returns true when queue is empty. */
+bool empty_q();
+
+/* Enqueues an item at the rear of queue. */
+void enqueue(Queue*, char);
+
+/* Dequeues the item at the front of queue, also returns the dequeued item. */
+char dequeue(Queue*);
+
+
+
+#endif
